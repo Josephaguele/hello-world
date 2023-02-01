55bad3584d86e651f0d4ca011e2816404c868eb9
Joseph Aguele <osekuemen@gmail.com>
2023-02-01 23:02:45 +0000

    basic event handling

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Welcome from './components/Welcome'
 import Hello from './components/hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          
-          <Greet name="Diana" heroName="Wonder Woman" />
-        }
+          <FunctionClick />
+          <ClassClick />
       </div>
     );
   }
